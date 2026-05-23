2509f91b36387b13881da8195c43a87d3034c140
Rohit Chornele <rohitchornele@gmail.com>
2026-05-23 15:12:53 +0530

    fix: cors origin

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -22,7 +22,7 @@ const openApiDocument = generateOpenApiDocument(serverRouter, {
 // if (env.NODE_ENV !== "production") {
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [ "http://localhost:3000" , "https://chaiforms-web.vercel.app"],
     credentials: true,
   }),
 );
