b1aba2bc06035a694b3f21933d0b7977cfc671f7
Rohit Chornele <rohitchornele@gmail.com>
2026-05-28 10:45:10 +0530

    fix: cookie issue

--- packages/trpc/server/utils/cookie.ts
+++ packages/trpc/server/utils/cookie.ts
@@ -1,7 +1,5 @@
-
-import type { Response, CookieOptions, Request } from 'express'
-import { TRPCContext } from '../context';
-
+import type { Response, CookieOptions, Request } from "express";
+import { TRPCContext } from "../context";
 
 const ONE_MINUTE = 60 * 1000;
 const ONE_HOUR = 60 * ONE_MINUTE;
@@ -9,51 +7,62 @@ const ONE_DAY = 24 * ONE_HOUR;
 const ONE_MONTH = 30 * ONE_DAY;
 const ONE_YEAR = 12 * ONE_MONTH;
 
+// const defaultCookieOption: CookieOptions = {
+//     path: "/",
+//     httpOnly: true,
+//     secure: true, // false in dev
+//     sameSite: "none",  //lax in dev
+//     maxAge: ONE_YEAR
+// }
 
 const defaultCookieOption: CookieOptions = {
-    path: "/",
-    httpOnly: true,
-    secure: true, // false in dev
-    sameSite: "none",  //lax in dev
-    maxAge: ONE_YEAR
-}
+  path: "/",
 
+  httpOnly: true,
+
+  secure: process.env.NODE_ENV === "production",
+
+  sameSite: "none",
+
+  domain: ".vercel.app",
+
+  maxAge: ONE_YEAR,
+};
 
 export function createCookieFactory(res: Response) {
-    return function createCookie(name: string, value: string, opts: CookieOptions = defaultCookieOption) {
-        res.cookie(name, value, opts)
-    }
-
+  return function createCookie(
+    name: string,
+    value: string,
+    opts: CookieOptions = defaultCookieOption,
+  ) {
+    res.cookie(name, value, opts);
+  };
 }
 
-
 export function getCookieFactory(req: Request) {
-    return function getCookie(name: string) {
-        return req.cookies?.[name]
-    }
-
+  return function getCookie(name: string) {
+    return req.cookies?.[name];
+  };
 }
 
 export function clearCookieFactory(res: Response) {
-    return function clearCookie(name: string) {
-        res.clearCookie(name);
-    }
-
+  return function clearCookie(name: string) {
+    res.clearCookie(name);
+  };
 }
 
 // Authentication cookie functions
 
-const AUTHENTICATION_COOKIE_NAME = "authentication-token"
+const AUTHENTICATION_COOKIE_NAME = "authentication-token";
 
 export function setAuthenticationCookie(ctx: TRPCContext, accessToken: string) {
-    ctx.createCookie(AUTHENTICATION_COOKIE_NAME, accessToken);
+  ctx.createCookie(AUTHENTICATION_COOKIE_NAME, accessToken);
 }
 
 export function getAuthenticationCookie(ctx: TRPCContext) {
-    return ctx.getCookie(AUTHENTICATION_COOKIE_NAME);
+  return ctx.getCookie(AUTHENTICATION_COOKIE_NAME);
 }
 
 export function clearAuthenticationCookie(ctx: TRPCContext) {
-    ctx.clearCookie(AUTHENTICATION_COOKIE_NAME);
+  ctx.clearCookie(AUTHENTICATION_COOKIE_NAME);
 }
-
