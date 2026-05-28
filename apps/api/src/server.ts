ffcd3404932d6d9779947731b5fca3ca4c0a3267
Rohit Chornele <rohitchornele@gmail.com>
2026-05-28 12:29:45 +0530

    fix: dashboard layout

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -22,7 +22,7 @@ const openApiDocument = generateOpenApiDocument(serverRouter, {
 // if (env.NODE_ENV !== "production") {
 app.use(
   cors({
-    origin: "https://chaiforms-web.vercel.app",
+    origin: ["https://chaiforms-web.vercel.app"],
     credentials: true,
   }),
 );
